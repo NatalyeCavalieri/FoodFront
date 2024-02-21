94adcf9765795fdc29ab9a6342d9d5b3b25c07ec
Natalye <cavalieridev@gmail.com>
2024-02-21 07:03:06 +0100

    finish create input text and init create input select

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -18,11 +18,26 @@ return (
       <Form>
         <InputContext>
           <p>Imagem do prato</p>
-          <label htmlFor="arquivo">
+          <label htmlFor="arquivo" className="label-file">
             <FiUpload size={24} />
             Selecione imagem
           </label>
           <input type="file" id="arquivo" />
+        </InputContext>
+        <InputContext>
+          <label className="label-text">Nome</label>
+          <input type="text" placeholder="Ex.: Salada Ceasar" />
+        </InputContext>
+        <InputContext>
+          <label htmlFor="category" className="label-text">
+            Categoria
+          </label>
+          <select name="category" id="category">
+            <option value="category1">Refeição</option>
+            <option value="category2">Prato principal</option>
+            <option value="category3">Bebidas</option>
+            <option value="category4">Sobremesa</option>
+          </select>
         </InputContext>
       </Form>
     </Content>
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -1,9 +1,6 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-
-export const Container = styled.div`
-
-`
+export const Container = styled.div``
 
 export const ButtonReturn = styled.div`
   display: flex;
@@ -30,52 +27,82 @@ export const ButtonReturn = styled.div`
     font-weight: 500;
     line-height: 3.3rem;
   }
-   `
+`
 
-   export const Content = styled.div`
-   border: 1px solid yellow;
-   margin-inline: 3.2rem;
-   height: 100vh;
-   width: 36.4rem;
+export const Content = styled.div`
+  border: 1px solid yellow;
+  margin-inline: 3.2rem;
+  height: 100vh;
+  width: 36.4rem;
 
-
-   h1{
+  h1 {
     margin-bottom: 2.4rem;
     font-size: 2.4rem;
-   }
-   `
+  }
+`
 
-   export const Form = styled.form`
-   width: 100%;
-   height: max-content;
+export const Form = styled.form`
+  width: 100%;
+  height: max-content;
 
-   input[type="file"] {
+  input[type="file"] {
     display: none;
-   }
+  }
 
-   label{
+  .label-file {
     padding: 1.2rem 3.2rem;
     background-color: ${({ theme }) => theme.COLORS.DARK_800};
     border-radius: 8px;
     margin-top: 1.6rem;
-    font-family: ${({theme})=> theme.FONTS.POPPINS};
+    font-family: ${({ theme }) => theme.FONTS.POPPINS};
     font-size: 1.4rem;
     display: flex;
     align-items: center;
 
-    >svg{
-      margin-right: .8rem;
+    > svg {
+      margin-right: 0.8rem;
     }
-   }
-   `
+  }
+`
 
-   export const InputContext = styled.div`
-    display: flex;
-    flex-direction: column;
+export const InputContext = styled.div`
+  display: flex;
+  flex-direction: column;
 
-    > p{
-      font-size: 1.6rem;
-      color: ${({theme})=> theme.COLORS.LIGHT_400};
+  > p {
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  }
+
+  input[type="text"] {
+    border: none;
+    background-color: ${({ theme }) => theme.COLORS.DARK_800};
+    border-radius: 8px;
+    margin-top: 1.6rem;
+    font-family: ${({ theme }) => theme.FONTS.POPPINS};
+    font-size: 1.4rem;
+    padding: 1.2rem 3.2rem;
+  }
+  .label-text {
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    font-family: ${({ theme }) => theme.FONTS.ROBOTO};
+    margin-top: 2.4rem;
+  }
+
+  select {
+    border: none;
+    background-color: ${({ theme }) => theme.COLORS.DARK_800};
+    border-radius: 8px;
+    margin-top: 1.6rem;
+    font-family: ${({ theme }) => theme.FONTS.ROBOTO};
+    font-size: 1.4rem;
+    padding: 1.2rem 3.2rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    
+
+    &:focus{
+      outline: none;
     }
-  
-   `
+  }
+`
